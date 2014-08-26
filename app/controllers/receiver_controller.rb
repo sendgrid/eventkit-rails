6e72ac66c1cb6a9b41c0dfec26cd8cc4d415d3e1
Scott K <scott.kawai@sendgrid.com>
2014-08-25 19:56:25 -0700

    Escape forward slashes when taking in data

--- app/controllers/receiver_controller.rb
+++ app/controllers/receiver_controller.rb
@@ -19,7 +19,11 @@ class ReceiverController < ApplicationController
 					if key == "type" then
 						props["type_id"] = value
 					elsif columns[key] then
-						props[key] = to_string(value)
+						str = to_string(value)
+						if str.is_a? String then
+							str = str.gsub(/\\\//,"/")
+						end
+						props[key] = str
 					else
 						unique_args[key] = to_string(value)
 					end
